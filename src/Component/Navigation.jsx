37be27968a8f05048a221cae84eb35894440d390
Abhishek jaiswal <115688230+Abhijaiswal2522002@users.noreply.github.com>
2024-06-27 04:30:40 +0530

    Initial commit

---
+++ src/Component/Navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation=()=>{
+return<div className="conatiner">
+    <nav>
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo"></img>
+      </div>
+      <ul>
+        <li href="#">Menu </li>
+        <li href="#">location</li>
+        <li href="#">About</li>
+        <li href="#">contact</li>
+      </ul>
+      <button>login</button>
+    </nav>
+</div>
+
+}
+export default Navigation
